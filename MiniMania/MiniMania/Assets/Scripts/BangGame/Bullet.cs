2828e56dff5b2b1a6c556bd861facc86350ce947
Eric Phangnanouvong <viruth.gmail@gmail.com>
2016-12-12 16:28:21 -0500

    bang game done

---
+++ MiniMania/MiniMania/Assets/Scripts/BangGame/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour {
+    public int direction;
+    public Text winText;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (direction == 1)
+        {
+            this.transform.Translate(Vector3.right*.5f);
+        }
+
+        if (direction == -1)
+        {
+            this.transform.Translate(Vector3.right*-.5f);
+        }
+
+    }
+}
