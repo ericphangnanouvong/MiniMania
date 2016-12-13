fe86af490fa58fa401bda4a42eccfd5e39443b6c
Eric Phangnanouvong <viruth.gmail@gmail.com>
2016-12-13 02:20:20 -0500

    game flow

--- MiniMania/MiniMania/Assets/Scripts/BalloonGame/BalloonSpawn.cs
+++ MiniMania/MiniMania/Assets/Scripts/BalloonGame/BalloonSpawn.cs
@@ -4,56 +4,22 @@ using System.Collections;
 public class BalloonSpawn : MonoBehaviour {
     
     public GameObject balloon;
-    public float initialSpawnTime;
-    public float repeatingSpawnTime;
-    public int balloonType;
+    public float spawnTime;
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-
 
     // Use this for initialization
     void Start () {
-        InvokeRepeating("Spawn", initialSpawnTime, repeatingSpawnTime);
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
     void Spawn()
     {
-
+       
         // Find a random index between zero and one less than the number of spawn points.
-          int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
         // Create an instance of the balloon prefab at the randomly selected spawn point's position and rotation.
-       //    Instantiate(balloon, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-        
-        if (balloonType == 1)
-        {
-            for (int i = 0; i < PoolManager.Instance.redballoonList.Count; i++)
-            {
-
-                if (PoolManager.Instance.redballoonList[i].activeInHierarchy == false)
-                {
-
-                    PoolManager.Instance.redballoonList[i].SetActive(true);
-                    PoolManager.Instance.redballoonList[i].transform.position = spawnPoints[spawnPointIndex].position;
-
-                    break;
-                }
-            }
-        }
-        if (balloonType == 2)
-        {
-            for (int i = 0; i < PoolManager.Instance.blueballoonList.Count; i++)
-            {
-
-                if (PoolManager.Instance.blueballoonList[i].activeInHierarchy == false)
-                {
-
-                    PoolManager.Instance.blueballoonList[i].SetActive(true);
-                    PoolManager.Instance.blueballoonList[i].transform.position = spawnPoints[spawnPointIndex].position;
-
-                    break;
-                }
-            }
-        }
-
+        Instantiate(balloon, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
+
 }
