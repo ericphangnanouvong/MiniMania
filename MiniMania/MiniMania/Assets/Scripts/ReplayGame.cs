ed89ed4bba2002c77a4f6f4d5165e475474d0459
Juan <Juan.Valdez@georgebrown.ca>
2017-04-05 00:44:40 -0400

    New Stats Scene and incomplete restart button.

---
+++ MiniMania/MiniMania/Assets/Scripts/ReplayGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayGame : MonoBehaviour {
+
+	private TextRandomizer newScene;
+	void Start () 
+	{	
+
+		newScene = GameObject.Find("Main Menu2").GetComponent<TextRandomizer>();
+
+		//newScene.originalGamesAdded = true;
+		SceneManager.LoadScene("Transition");
+	}
+	
+	public void TransitionToScene (string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+
+
+}
